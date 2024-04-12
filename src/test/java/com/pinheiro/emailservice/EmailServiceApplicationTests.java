08803733e9e2a44aea8ba3a742aaacb7cb800d6e
Guilherme Alves Pinheiro <guiapinheiro29@gmail.com>
2024-04-12 12:49:35 -0300

    initial commit

---
+++ src/test/java/com/pinheiro/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinheiro.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
